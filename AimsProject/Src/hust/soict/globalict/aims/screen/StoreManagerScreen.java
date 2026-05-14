c469e1d917e35e165e2ea721cbdded5cd53323c6
hieuvan153 <hieuvan153282278@gmail.com>
2026-05-14 13:58:31 +0700

    feat: add view store screen

--- AimsProject/Src/hust/soict/globalict/aims/screen/StoreManagerScreen.java
+++ AimsProject/Src/hust/soict/globalict/aims/screen/StoreManagerScreen.java
@@ -1,5 +1,8 @@
 package hust.soict.globalict.aims.screen;
 
+import hust.soict.globalict.aims.media.Book;
+import hust.soict.globalict.aims.media.CompactDisc;
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
 import hust.soict.globalict.aims.media.Media;
 import hust.soict.globalict.aims.store.Store;
 
@@ -8,8 +11,23 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.concurrent.Flow;
 
-public class StoreManagerScreen {
+public class StoreManagerScreen extends JFrame {
     private Store store;
+
+    public StoreManagerScreen(Store store) {
+        this.store = store;
+
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(createNorth(), BorderLayout.NORTH);
+        cp.add(createCenter(), BorderLayout.CENTER);
+
+        setTitle("Store");
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
 
     JPanel createNorth() {
         JPanel north = new JPanel();
@@ -57,11 +75,30 @@ public class StoreManagerScreen {
         center.setLayout(new GridLayout(3, 3, 2, 2));
 
         ArrayList<Media> mediaInStore = store.getItemsInStore();
-        for (int i = 0; i <= 9; i++) {
+        int limit = Math.min(mediaInStore.size(), 9);
+        for (int i = 0; i < limit; i++) {
             MediaStore cell = new MediaStore(mediaInStore.get(i));
             center.add(cell);
         }
 
         return center;
+    }
+
+    public static void main(String[] args) {
+        Store store = new Store();
+
+        store.addMedia(new DigitalVideoDisc("Harry Potter and the Philosopher's Stone (2001)", "Fantasy", "Chris Columbus", 152, 3.0f));
+        store.addMedia(new DigitalVideoDisc("Harry Potter and the Chamber of Secrets (2002)", "Fantasy", "Chris Columbus", 161, 3.5f));
+        store.addMedia(new DigitalVideoDisc("Harry Potter and the Prisoner of Azkaban (2004)", "Fantasy", "Alfonso Cuarón", 142, 5.0f));
+        store.addMedia(new DigitalVideoDisc("Harry Potter and the Goblet of Fire (2005)", "Fantasy", "Mike Newell", 157, 4.5f));
+
+        store.addMedia(new CompactDisc(1, "Fetch the Bolt Cutters", "Music", "Fiona Apple", "Unknown", 10.39f));
+        store.addMedia(new CompactDisc(2, "Future Nostalgia", "Music", "Dua Lipa", "Unknown", 9.6f));
+
+        store.addMedia(new Book(1, "The Hunger Games", "Young Adult", 5.5f));
+        store.addMedia(new Book(2, "Catching Fire", "Young Adult", 4.9f));
+        store.addMedia(new Book(3, "Mockingbird", "Young Adult", 5.1f));
+
+        new StoreManagerScreen(store);
     }
 }
