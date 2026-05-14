3ed92e6aea3e0556ce7a3c824b089d48cd392d3b
hieuvan153 <hieuvan153282278@gmail.com>
2026-05-14 10:19:59 +0700

    feat: Create numbergrid.java

---
+++ GUIProject/hust/soict/globalict/swing/NumberGrid.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NumberGrid extends JFrame {
+    private JButton[] btnNumbers = new JButton[10];
+    private JButton btnDelete, btnReset;
+    private JTextField tfDisplay;
+
+    public NumberGrid() {
+        tfDisplay = new JTextField();
+        tfDisplay.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        JPanel panelButtons = new JPanel(new GridLayout(4, 3));
+        addButtons(panelButtons);
+
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(tfDisplay, BorderLayout.NORTH);
+        cp.add(panelButtons, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("NumberGrid");
+        setSize(200, 200);
+        setVisible(true);
+    }
+}
