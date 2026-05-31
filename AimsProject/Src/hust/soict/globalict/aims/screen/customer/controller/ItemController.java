11e88520d5518412e155a7d9dd8aaf3f95dc8807
hieuvan153 <hieuvan153282278@gmail.com>
2026-05-31 18:03:14 +0700

    feat: Created ItemController

---
+++ AimsProject/Src/hust/soict/globalict/aims/screen/customer/controller/ItemController.java
@@ -0,0 +1,87 @@
+package hust.soict.globalict.aims.screen.customer.controller;
+
+import hust.soict.globalict.aims.cart.Cart;
+import hust.soict.globalict.aims.media.Media;
+import hust.soict.globalict.aims.media.Playable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import javax.naming.LimitExceededException;
+
+public class ItemController {
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Label lblCost;
+
+    @FXML
+    private Label lblTitle;
+
+    private Media media;
+    private Cart cart;
+
+    public ItemController(Cart cart) {
+        this.cart = cart;
+    }
+
+    public void setData(Media media) {
+        this.media = media;
+        lblTitle.setText(media.getTitle());
+        lblCost.setText(media.getCost() + "$");
+        if (media instanceof Playable) {
+            btnPlay.setVisible(true);
+        } else {
+            btnPlay.setVisible(false);
+            HBox.setMargin(btnAddToCart, new Insets(0, 0, 0, 60));
+        }
+    }
+
+    @FXML
+    void btnAddToCartClicked(ActionEvent event) {
+        if (media == null || cart == null) {
+            return;
+        }
+
+        try {
+            cart.addMedia(media);
+            showInfo("Added to cart", "\"" + media.getTitle() + "\" has been added to your cart.");
+        } catch (LimitExceededException ex) {
+            showError("Cart Full", ex.getMessage());
+        }
+    }
+
+    @FXML
+    void btnPlayClicked(ActionEvent event) {
+        if (media instanceof Playable) {
+            ((Playable) media).play();
+            showInfo("Playing", "Now playing" + media.getTitle());
+        } catch (PlayerException ex) {
+            showError("Playback Error", ex.getMessage());
+        }
+    }
+
+    private void showInfo(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void showError(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+}
