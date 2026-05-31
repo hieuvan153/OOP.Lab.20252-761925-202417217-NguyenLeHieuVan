d715315697ff0c367b5e0e3f5ff638ceee6ee761
hieuvan153 <hieuvan153282278@gmail.com>
2026-05-31 09:19:21 +0700

    Save progress on Painter UI and Controller

--- GUIProject/hust/soict/globalict/javafx/PainterController.java
+++ GUIProject/hust/soict/globalict/javafx/PainterController.java
@@ -1,4 +1,29 @@
 package hust.soict.globalict.javafx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class PainterController {
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private RadioButton eraserRadio;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+        Color inkColor = eraserRadio.isSelected() ? Color.WHITE : Color.BLACK;
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, inkColor);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
 }
